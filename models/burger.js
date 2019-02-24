1eaa3a57a205442b2fda5830b4c944232f15bf86
rknucks <42450529+rknucks@users.noreply.github.com>
2019-02-23 20:35:21 -0700

    fixed burger.js

---
+++ models/burger.js
@@ -0,0 +1,33 @@
+var orm = require('../config/orm.js');
+
+// Call the ORM function
+var burger = 
+{
+
+  selectAll: function(callback)
+  {
+    orm.selectAll(function(res)
+    {
+      callback(res);
+    });
+  },
+
+  insertOne: function(burger_name, callback)
+  {
+    orm.insertOne(burger_name, function(res)
+    {
+      callback(res);
+    });
+  },
+
+  updateOne: function(burger_id, callback)
+  {
+    orm.updateOne(burger_id, function(res)
+    {
+      callback(res);
+    });
+  }
+
+};
+
+module.exports = burger;
