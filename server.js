dc582a517353cbb05227146a45e0779a32ea69c1
rknucks <42450529+rknucks@users.noreply.github.com>
2019-02-26 18:15:45 -0700

    trying

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-var methodOverride = require('method-override');
+// var methodOverride = require('method-override');
 
 
-var PORT = process.env.PORT || 8080;
+var PORT = process.env.port || 8080;
 
 var app = express();
 
 app.use(express.static(__dirname + '/public'));
 
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // Handlebars
 var exphbs = require('express-handlebars');
@@ -22,7 +22,6 @@ app.use(router);
 
 
 
-app.listen(port, function() 
-{
+app.listen(PORT, function() {
   console.log("App listening on PORT " + PORT);
 });
